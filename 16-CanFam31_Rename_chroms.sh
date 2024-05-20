3118a8da8b485edf87ddb9526f56640096f95ec7
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2024-05-20 14:51:57 -0600

    Update 16-CanFam31_Rename_chroms.sh
    Added additional comments describing the purpose of the script and modified ntasks and time to reflect what worked when I ran this on Alpine.

--- 16-CanFam31_Rename_chroms.sh
+++ 16-CanFam31_Rename_chroms.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
+# Purpose: To rename chromosomes in the reference vcf file to a compatible format
+# NOTE: This script must be run in a conda environment with bcftools installed
 
 #SBATCH --job-name=RenameChrom
 
 #SBATCH --nodes=1
-#SBATCH --ntasks=4       # modify this number to reflect how many cores you want to use (up to 24)
-#SBATCH --time=4:00:00   # set time; default = 4 hours
+#SBATCH --ntasks=32       # modify this number to reflect how many cores you want to use (up to 24)
+#SBATCH --time=8:00:00   # set time; default = 4 hours
 #SBATCH --partition=amilan
 #SBATCH --qos=normal
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT   # Keep these two lines of code if you want an e-mail sent to you when it is complete.
 #SBATCH --mail-user=edlarsen@colostate.edu
 #SBATCH --output=RenameChrom_%j.txt  # this will capture all output in a logfile with %j as the job #
 
-
-
-## Execute this script in a conda environment containing gatk4, or un-comment and run the following 2 lines of code: 
-#module purge ## purge all existing modules
-#module load gatk
 
 bcftools annotate --rename-chrs Renam_chromosomes_file.txt broad_umass_canid_variants.1.2.vcf.gz | bgzip > rename.broad_umass_canid_variants.1.2.vcf.gz
