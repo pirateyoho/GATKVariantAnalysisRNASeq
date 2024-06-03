bfe0709ba2917f025aa8d7d9c87be235559d85f6
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2024-06-03 15:16:56 -0600

    Update 34-fixVepFormat.sh

--- 34-fixVepFormat.sh
+++ 34-fixVepFormat.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Purpose: To fix the format of Ensembl VEP vcf files to a format that the vcf2maf tool can read
-# This script should be run in the directory containing the vep.vcf files output by Ensemble VEP
+# This script should be run locally in the directory containing the vep.vcf files output by Ensemble VEP
 
 # Loop through vep.vcf files in the current directory and fix format of each
 for file in *.vep.vcf; do name=$(basename ${file} .vep.vcf); echo ${name};
