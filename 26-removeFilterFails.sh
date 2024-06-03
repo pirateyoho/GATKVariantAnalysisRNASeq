db41c184b30a5e9995d55342d503af5371059515
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2024-06-03 14:33:31 -0600

    Add files via upload

---
+++ 26-removeFilterFails.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Purpose: To remove variants that failed the filters specified in gatk VariantFiltration
+
+# make output directory
+mkdir PassedFiltering
+
+# loop through each maf file in the current directory and keep only the lines that do not contain "my_filter1"
+for file in *.vcf; do name=$(basename ${file} .vcf);
+bcftools view -i 'FILTER="PASS"' ${file} > PassedFiltering/${name}.pass.vcf
+done
