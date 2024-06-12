387fdf5016c581ab4c12601ff868920d63dc29e9
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2024-06-12 10:58:27 -0600

    Update 37-filterMAFs.sh

--- 37-filterMAFs.sh
+++ 37-filterMAFs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Purpose: To filter intronic variants and variants predicted to have modifier effect in a maf file, to reduce the size of the maf file before merging and downstream analysis.
+# Purpose: To filter intronic and splite site variants and variants predicted to have low modifier effects in a maf file, to reduce the size of the maf file before merging and downstream analysis.
 # Should be run after Ensembl Variant Effect Predictor tool and vcf2maf
 
 # make output directory
 mkdir FilteredMAFs
 
-# loop through each maf file in the current directory and keep only the lines that do not contain the pattern "Intron" or "MODIFIER"
+# loop through each maf file in the current directory and keep only the lines that do not contain the pattern "Intron", "MODIFIER", "Splice_Site", "Splice_Region", or "LOW"
 for file in *.vep.maf; do name=$(basename ${file} .maf);
-grep -v -e "Intron" -e "MODIFIER" ${file} > FilteredMAFs/${name}.filtered.maf
+grep -v -e "Intron" -e "Splice_Site" -e "Splice_Region" -e "MODIFIER" -e "LOW" ${file} > FilteredMAFs/${name}.filtered.maf
 done
