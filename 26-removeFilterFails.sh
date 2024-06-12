87752fdfd55518e925ec64bd8f5dddfdaf6fe05e
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2024-06-12 10:45:57 -0600

    Update 26-removeFilterFails.sh
    Fixed typo where maf was used instead of vcf

--- 26-removeFilterFails.sh
+++ 26-removeFilterFails.sh
@@ -4,7 +4,7 @@
 # make output directory
 mkdir PassedFiltering
 
-# loop through each maf file in the current directory and keep only the lines that do not contain "my_filter1"
+# loop through each vcf file in the current directory and keep only the lines that do not contain "my_filter1"
 for file in *.vcf; do name=$(basename ${file} .vcf);
 bcftools view -i 'FILTER="PASS"' ${file} > PassedFiltering/${name}.pass.vcf
 done
