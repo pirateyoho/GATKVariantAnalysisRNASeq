535904ba3f9ac8d3c58d0f68405be83344d57164
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2024-06-12 10:45:22 -0600

    Update 25-variantFiltration_snp.sh
    Removed QUAL 30 filter

--- 25-variantFiltration_snp.sh
+++ 25-variantFiltration_snp.sh
@@ -14,13 +14,12 @@
  start=`date +%s`
 
 
-#Run VariantFiltration with basic filtering thresholds from the GATK documentation: https://gatk.broadinstitute.org/hc/en-us/articles/360035531112--How-to-Filter-variants-either-with-VQSR-or-by-hard-filtering
+#Run VariantFiltration with basic filtering thresholds from the GATK documentation: https://gatk.broadinstitute.org/hc/en-us/articles/360035890471-Hard-filtering-germline-short-variants
 for file in *snps.vcf; do name=$(basename ${file} .raw_snps.vcf); echo ${name};
 gatk VariantFiltration \
 -R ../../../../../../Input/GenomeData/CanFam31_FullGenome.fa \
 -V ${file} \
 -filter "QD < 2.0" --filter-name "QD2" \
--filter "QUAL < 30.0" --filter-name "QUAL30" \
 -filter "SOR > 3.0" --filter-name "SOR3" \
 -filter "FS > 60.0" --filter-name "FS60" \
 -filter "MQ < 40.0" --filter-name "MQ40" \
