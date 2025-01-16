cf5682a5499ea71cd4c5b5c43743b6ab03f98feb
Eileen O. DVM DACVP <134553786+pirateyoho@users.noreply.github.com>
2025-01-16 15:33:25 -0700

    Update 39-lollipop-plots.sh

--- 39-lollipop-plots.sh
+++ 39-lollipop-plots.sh
@@ -6,10 +6,10 @@ tar -xzf lollipops_1.7.2_linux_amd64.tar.gz
 
 # Space-delimited list of mutations for 1 gene from maf file
 ## Format: <AMINO><CODON><AMINO><#COLOR><@COUNT>
-mutations="E230K M1571Ifs*52@3 C1368W Q743H Y2005H R1224P"
+mutations="V48A E198K@2 A439T V669A V744M W780R@3 C846Y"
 
 # Space-delimited list of Uniprot IDs for that gene
-uniprots="A0A8P0NCQ6 A0A8I3Q1L1 A0A8I3Q267 A0A8P0T2E3 A0A8C0PHT6 A0A8C0T0I2"
+uniprots="A0A8C0MAP0 A0A8I3SA17 A0A8C0QMZ5"
 
 #### Draw plots ####
 ## Format: lollipops [options] {-U UNIPROT_ID | GENE_SYMBOL} [PROTEIN CHANGES ...]
